4a68aa41f483d4742b1710ebe37a85acdb339980
Vlad Stan <stan.v.vlad@gmail.com>
2022-08-03 17:39:13 +0300

    feat: update help

--- wallet/100_constants.ino
+++ wallet/100_constants.ino
@@ -1,5 +1,6 @@
 const int DELAY_MS = 5;
 
+const String COMMAND_HELP = "/help";
 const String COMMAND_RESTORE = "/restore";
 const String COMMAND_WIPE = "/wipe";
 const String COMMAND_PASSWORD = "/password";
@@ -7,7 +8,8 @@ const String COMMAND_PASSWORD_CLEAR = "/password-clear";
 const String COMMAND_SEED = "/seed";
 const String COMMAND_SEND_PSBT = "/psbt";
 const String COMMAND_SIGN_PSBT = "/sign";
+const String COMMAND_XPUB = "/xpub";
 const String COMMAND_CONFIRM_NEXT = "/confirm-next";
 const String COMMAND_CANCEL = "/cancel";
-const String COMMAND_HELP = "/help";
-const String COMMAND_XPUB = "/xpub";
+
+
