a98867c6a47a18bb6eff363fe20838c7458c7204
Vlad Stan <stan.v.vlad@gmail.com>
2022-08-15 12:28:08 +0300

    refactor: remove dead code

--- wallet/600_psbt.ino
+++ wallet/600_psbt.ino
@@ -5,37 +5,6 @@ PSBT parseBase64Psbt(String psbtBase64) {
   return psbt;
 }
 
-// todo: remove
-void printPsbtDetails(PSBT psbt, HDPrivateKey hd) {
-
-  // check parsing is ok
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  tft.setTextSize(1);
-  tft.setCursor(0, 30);
-  tft.println("Transactions details:");
-  // going through all outputs
-  tft.println("Outputs:");
-  for (int i = 0; i < psbt.tx.outputsNumber; i++) {
-    // print addresses
-    tft.print(psbt.tx.txOuts[i].address(&Mainnet)); // todo: support testnet
-    if (psbt.txOutsMeta[i].derivationsLen > 0) { // there is derivation path
-      // considering only single key for simplicity
-      PSBTDerivation der = psbt.txOutsMeta[i].derivations[0];
-      HDPublicKey pub = hd.derive(der.derivation, der.derivationLen).xpub();
-      if (pub.address() == psbt.tx.txOuts[i].address()) {
-        tft.print(" (change) ");
-      }
-    }
-    tft.print(" -> ");
-    tft.print(psbt.tx.txOuts[i].btcAmount() * 1e3);
-    tft.println(" mBTC");
-  }
-  tft.print("Fee: ");
-  tft.print(float(psbt.fee()) / 100); // Arduino can't print 64-bit ints
-  tft.println(" bits");
-}
-
 void printOutputDetails(PSBT psbt, HDPrivateKey hd, int index, const Network * network) {
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
