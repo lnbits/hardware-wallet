be2ff18dae49e1d64b2ba23829644462c68cee2a
Vlad Stan <stan.v.vlad@gmail.com>
2022-08-18 08:39:38 +0300

    feat: add `ping` and `check_pairing` commands

--- wallet/800_setup.ino
+++ wallet/800_setup.ino
@@ -15,7 +15,7 @@ void setup() {
 
   logSerial("HWW: waiting for commands");
   // In case of forced reboot, tell the client to logout.
-  // Secure connection not established yet. Sengin in clear text.
+  // Secure connection not established yet. Sendin in clear text.
   Serial.println(COMMAND_PASSWORD_CLEAR +  " 1");
 
   if (loadFiles() == false)
@@ -24,16 +24,38 @@ void setup() {
 }
 
 bool loadFiles() {
-  FileData pwdFile = readFile(SPIFFS, "/hash.txt");
+  FileData pwdFile = readFile(SPIFFS, global.passwordFileName.c_str());
   String passwordHash = pwdFile.data;
 
   int byteSize =  passwordHash.length() / 2;
   byte passwordHashBin[byteSize];
   fromHex(passwordHash, passwordHashBin, byteSize);
 
-  FileData mnFile = readFile(SPIFFS, "/mn.txt");
+  FileData mnFile = readFile(SPIFFS, global.mnemonicFileName.c_str());
   global.encrytptedMnemonic = decryptDataWithIv(passwordHashBin, mnFile.data);
   global.passwordHash = passwordHash;
+
+  FileData sharedSecretFile = readFile(SPIFFS, global.sharedSecretFileName.c_str());
+  logSerial("sharedSecretFile.success " + String(sharedSecretFile.success));
+  if (sharedSecretFile.success) {
+    logSerial("sharedSecretFile.data " + String(sharedSecretFile.data));
+    fromHex(sharedSecretFile.data, global.dhe_shared_secret, sizeof(global.dhe_shared_secret));
+  }
+
+  FileData deviceMetaFile = readFile(SPIFFS, global.deviceMetaFileName.c_str());
+  logSerial("deviceMetaFile.success " + String(deviceMetaFile.success));
+  if (deviceMetaFile.success) {
+    logSerial("sharedSecretFile.data " + deviceMetaFile.data);
+    global.deviceUUID = deviceMetaFile.data;
+  } else {
+    // create random unique ID
+    int uuidSize = 32;
+    uint8_t uuid[uuidSize];
+    String tempMnemonic = generateMnemonic(24);
+    mnemonicToEntropy(tempMnemonic, uuid, uuidSize);
+    global.deviceUUID = toHex(uuid, uuidSize);
+    writeFile(SPIFFS, global.deviceMetaFileName.c_str(), global.deviceUUID);
+  }
 
   return mnFile.success && pwdFile.success;
 }
