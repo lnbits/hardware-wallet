762fdce838bbf402fbde30f302b113069c0303ed
Vlad Stan <stan.v.vlad@gmail.com>
2022-09-27 11:37:38 +0300

    fix: log method

--- wallet/711_cmd_help.ino
+++ wallet/711_cmd_help.ino
@@ -4,13 +4,13 @@ CommandResponse executeHelp(String commandData) {
   const String testFileName = "/abc.txt";
   const String testFileNameOut = "/abc-out-8.txt";
   FileData fd = readFile(SD, testFileName.c_str());
-  logSerial("fd success: " + String(fd.success));
-  logSerial("fd data: " + fd.data);
+  logInfo("fd success: " + String(fd.success));
+  logInfo("fd data: " + fd.data);
   String line = "";
   writeFile(SD, testFileNameOut.c_str(), "IN " + testFileNameOut + "\n");
   int lineNumber = 0;
   do {
-    logSerial("line-number[" + String(lineNumber) + "]:");
+    logInfo("line-number[" + String(lineNumber) + "]:");
     line = getLineAtPosition(fd.data, lineNumber);
     const String l2 = String("["+String(lineNumber) + "]: " + line);
     line.toLowerCase();
@@ -18,7 +18,7 @@ CommandResponse executeHelp(String commandData) {
     lineNumber++;
   } while (line != ""); // last line not read!
 
-  logSerial("line-last[" + String(lineNumber) + "]: " + line);
+  logInfo("line-last[" + String(lineNumber) + "]: " + line);
   // appendFile(SD, testFileNameOut.c_str(), "OUT".c_str());
 
   return {"More info at:", "github.com/lnbits/hardware-wallet"};
@@ -44,15 +44,15 @@ void help()
   tft.println("'/sign' will sign valid psbt");
   tft.println("'/xpub' will return the XPub for a provided derivation path");
 
-  logSerial("Commands");
-  logSerial("'/help' show available commands");
-  logSerial("'/restore <BIP39 seed words seperated by space>' will restore from seed");
-  logSerial("'/wipe' will completely wipe device and creat a new wallet");
-  logSerial("'/password' login by providing a password");
-  logSerial("'/password-clear' logout");
-  logSerial("'/seed' will show the seed on the hww display");
-  logSerial("'/psbt' will parse valid psbt and show its outputs and fee");
-  logSerial("'/sign' will sign valid psbt");
-  logSerial("'/xpub' will return the XPub for a provided derivation path");
+  logInfo("Commands");
+  logInfo("'/help' show available commands");
+  logInfo("'/restore <BIP39 seed words seperated by space>' will restore from seed");
+  logInfo("'/wipe' will completely wipe device and creat a new wallet");
+  logInfo("'/password' login by providing a password");
+  logInfo("'/password-clear' logout");
+  logInfo("'/seed' will show the seed on the hww display");
+  logInfo("'/psbt' will parse valid psbt and show its outputs and fee");
+  logInfo("'/sign' will sign valid psbt");
+  logInfo("'/xpub' will return the XPub for a provided derivation path");
   delay(10000);
 }
