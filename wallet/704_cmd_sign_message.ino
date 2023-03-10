8d867412374888becc706aa266ae633abe37ed70
Vlad Stan <stan.v.vlad@gmail.com>
2023-03-10 10:13:27 +0200

    refactor: some file renamings

---
+++ wallet/704_cmd_sign_message.ino
@@ -0,0 +1,10 @@
+/**
+   @brief Sign a random message.
+
+   @param messageHex: String. The message to be signed in hex format.
+   @return CommandResponse
+*/
+CommandResponse executeSignMessage(String commandData) {
+
+  return {"Signed", "some message"};
+}
